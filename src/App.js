c3498c6fee75c10371839792845a29ac64cd4849
ankur-2103 <85174725+ankur-2103@users.noreply.github.com>
2023-04-28 12:14:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import Transcript from "./Components/Transcript";
 
 function App() {
+
+  const style = {
+    background: "linear-gradient(90deg, rgba(66,37,148,1) 0%, rgba(59,59,246,1) 48%, rgba(220,0,193,1) 100%)",
+    padding: "20px"
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={style}>
       <Transcript/>
     </div>
   );
--- src/Components/Transcript.jsx
+++ src/Components/Transcript.jsx
@@ -5,37 +5,72 @@ import './Transcript.css'
 
 const Transcript = () => {
 
+    //refernce for audio player
     const playerRef = useRef(null);
+
+    //refernce for word
     const wordsRef = useRef(null);
-    var a;
 
     useEffect(() => {
-
+        
+        var a;
+        
+        //function for timeupdate event listner
         const onTimeUpdate = () => {
+
+            //Get the index of current word
             const activeWordIndex = transcript.findIndex((word) => {
                 return word.data.start >= playerRef.current.currentTime;
             });
+
+            //Get the reference of current word
             const wordElement = wordsRef.current.childNodes[activeWordIndex-1];
             const wordElement2 = wordsRef.current.childNodes[a - 1];
+
+            //update the classname for current word
             try {
                 wordElement.classList.add('active-word');
+            } catch (error) {
+                console.log(error)
+            }
+
+            //update the classname for previous word
+            try {
                 wordElement2.classList.remove('active-word');
             } catch (error) {
                 console.log(error)
             }
+
+            //store the index of previous word
             a = activeWordIndex
         }     
         
+        //add timeupdate event listner to player
         playerRef.current.addEventListener("timeupdate", onTimeUpdate);
-        return () => playerRef.current.removeEventListener("timeupdate",onTimeUpdate);
+
+        //remove timeupdate event listner from player
+        const removeRef = () => playerRef.current.removeEventListener("timeupdate",onTimeUpdate);
+        
+        return () => removeRef()
+
     }, []);
 
+    //style for div
+    const styleDiv = {
+        display: "flex",
+        flexDirection: "column",
+        padding: "10px",
+        alignItems: "center",
+        borderRadius: "25px",
+        background: '#f0eff3'
+    }
+
     return (
-        <div style={{display:"flex", flexDirection:"column", padding:"20px", alignItems:"center"}}>
+        <div style={styleDiv}>
             <span ref={wordsRef}>
-                {transcript.map((word, i) => <span key={i} className=''>{word.data.text+" "}</span>)}
+                {transcript.map((word, i) => <span key={i} style={{padding:"1px"}}>{word.data.text+" "}</span>)}
             </span>
-            <audio controls src={faudio} ref={playerRef} style={{margin:"20px"}}/>;
+            <audio controls src={faudio} ref={playerRef} style={{margin:"20px", borderWidth:'2px'}}/>;
         </div>
     )
 }
